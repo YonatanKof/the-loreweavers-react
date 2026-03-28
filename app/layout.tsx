7d21e85d335c320838070163cab5b4978443428e
Yonatan Kof <kof@yonatankof.com>
2026-03-28 18:27:13 +0300

    feat: tweaking design and social stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,37 @@
 import type { Metadata } from 'next';
-import { Cinzel } from 'next/font/google';
+import { Rubik, Rubik_Wet_Paint } from 'next/font/google';
 import { Analytics } from "@vercel/analytics/next"
 import './globals.css';
 
-const cinzel = Cinzel({
-	subsets: ['latin'],
-	weight: ['400', '600'],
-	variable: '--font-cinzel',
+const rubik  = Rubik({
+	weight: ['300', '400', '500', '600', '700', '800', '900'],
+	variable: '--font-rubik ',
+	display: 'swap',
+});
+
+const rubikWetPaint  = Rubik_Wet_Paint({
+	subsets: ['hebrew'],
+	weight: ['400'],
+	variable: '--font-rubik-wet-paint ',
 	display: 'swap',
 });
 
 export const metadata: Metadata = {
 	title: 'The Loreweavers',
 	description: 'A chronicle of our D&D sessions.',
+	openGraph: {
+		images: ['/the-loreweavers.jpg'],
+	},
+	twitter: {
+		card: 'summary_large_image',
+		images: ['/the-loreweavers.jpg'],
+	},
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="he" dir="rtl" className={cinzel.variable}>
-			<body>{children}</body>
+		<html lang="he" dir="rtl" className={`${rubikWetPaint.variable} ${rubik.variable}`}>
+			<body>{children}<Analytics /></body>
 		</html>
 	);
 }
